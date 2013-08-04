09ee5b48ea0a98df1c7c6c0802481b0d740a335b
Vlasta Neubauer <paranoiq@centrum.cz>
2013-08-04 10:25:11 +0200

    SqlConventional: fix - getting table name from repository class does not depend on namespace

--- Skeleton/Orm/SqlConventional.php
+++ Skeleton/Orm/SqlConventional.php
@@ -17,6 +17,7 @@ class SqlConventional extends Orm\SqlConventional
 
 	/**
 	 * Formátuje jména tabulek (slova oddělená podtržítky, lowercase)
+	 *
 	 * @param Orm\IRepository
 	 * @return string
 	 */
@@ -25,12 +26,16 @@ class SqlConventional extends Orm\SqlConventional
 		$class = get_class($repository);
 		if (!isset($this->tableCache[$class]))
 		{
-			// odstraňujeme namespace + poslední lomítko a pak slovo "Repository"
+			// odstraňujeme namespace a slovo "Repository"
+			$pos = strrpos($class, '\\');
+			$name = substr($class, $pos + 1, strlen($class) - $pos - 11);
+
+			// převádíme cammel case na snake case
 			$this->tableCache[$class] = strtolower(
 				preg_replace('/([A-Z]+)([A-Z])/','\1_\2',
-					preg_replace('/([a-z\d])([A-Z])/','\1_\2',
-						substr($class, strlen(__NAMESPACE__) + 1, - strlen('Repository')))));
+					preg_replace('/([a-z\d])([A-Z])/','\1_\2', $name)));
 		}
+
 		return $this->tableCache[$class];
 	}
 
